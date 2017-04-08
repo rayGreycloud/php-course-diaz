b81b7d20ab114adb8f52b8d87d49feff57182595
rayGreycloud <rayGreycloud@gmail.com>
2017-04-08 07:24:44 -0700

    lecture 11 arrays

---
+++ arrays.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+  $numberList = array(23,64,267,478,'5345', '<h1>Hello</h1>');
+
+print_r($numberList[5]);
+?>
+
+
+
+</body>
+</html>

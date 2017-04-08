5f659fe3f9854c53ce31851edf46ed18de045ef8
rayGreycloud <rayGreycloud@gmail.com>
2017-04-08 07:23:37 -0700

    lecture 7 inserting comments in PHP

---
+++ comments.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+// This is an example of a one-line comment  
+
+/* This is an example of
+a multi-line comment */
+  echo "Imagine lots of code...";
+
+?>
+
+
+</body>
+</html>

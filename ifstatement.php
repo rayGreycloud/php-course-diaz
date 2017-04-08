000d0a74a703399e4295d765406f04f43eb54363
rayGreycloud <rayGreycloud@gmail.com>
2017-04-08 10:07:39 -0700

    lecture 14 if statements

---
+++ ifstatement.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+$a = 3;
+$b = 10;
+
+if($a > $b) {
+  echo "a is greater than b";
+} else if ($a < $b){
+  echo "a is less than b";
+} else {
+  echo "a equals b";
+}
+
+
+?>
+
+
+</body>
+</html>

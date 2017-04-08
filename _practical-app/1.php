64d5f7944d5c00a9c18d67051b4f4cc445a6225b
rayGreycloud <rayGreycloud@gmail.com>
2017-04-08 08:15:36 -0700

    Add practice sections folder and boilerplate

---
+++ _practical-app/1.php
@@ -0,0 +1,25 @@
+<?php include "functions.php"; ?>
+<?php include "includes/header.php";?>
+
+	<section class="content">
+
+	<aside class="col-xs-4">
+
+	<?php Navigation();?>
+
+
+	</aside><!--SIDEBAR-->
+
+
+<article class="main-content col-xs-8">
+
+<?php
+// Render 'Hello' to the browser
+  echo "<h1>Hello</h1>";
+?>
+
+
+
+		</article><!--MAIN CONTENT-->
+
+<?php include "includes/footer.php"; ?>

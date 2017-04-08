6596d888ed281ded8b999c38f92892c02cd6e1ca
rayGreycloud <rayGreycloud@gmail.com>
2017-04-08 07:24:26 -0700

    lecture 10 math

---
+++ math.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+echo 56 + 45;
+echo "<br>";
+$number1 = 10;
+$number2 = 40;
+echo $number1 + $number2;
+
+?>
+
+</body>
+</html>

cce4a6cebf07c148cdf8d6016bc5f1a76fbf5492
rayGreycloud <rayGreycloud@gmail.com>
2017-04-08 07:24:11 -0700

    lecture 9 variables in PHP

---
+++ variables.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+$name = "Edwin";
+$number = 100;
+
+// concatenate with ' . '
+echo $name . " " . $number;
+
+$hello = "<h1>Hello</h1>";
+echo $hello;
+?>
+
+
+
+</body>
+</html>

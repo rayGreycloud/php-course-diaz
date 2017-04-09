1a07f3d9074abbfb427adc57520b711ae5c72e0e
rayGreycloud <rayGreycloud@gmail.com>
2017-04-09 08:09:01 -0700

    lecture 19 foreach loops

---
+++ foreach_loop.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>For Loops</title>
+</head>
+<body>
+<?php
+$numbers = [1, 2, 3, 4, 5];
+foreach ($numbers as $number) {
+  echo "Count: " . $number . "<br>";
+
+}
+
+$letters = array("a", "b", "c", "d", "e");
+foreach ($letters as $letter) {
+  echo "Letter: " . $letter . "<br>";
+
+}
+
+?>
+</body>
+</html>

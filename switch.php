cd47082ec66ebf3a6116d99104b4f7df2e50588b
rayGreycloud <rayGreycloud@gmail.com>
2017-04-09 07:17:02 -0700

    lecture 16 switch statements

---
+++ switch.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+$a = 3;
+$b = 10;
+
+switch($b) {
+  case 1:
+  echo "It's 1";
+  break;
+  case 2:
+  echo "It's 2";
+  break;
+  case 3:
+  echo "It's 3";
+  break;
+  case 4:
+  echo "It's 4";
+  break;
+  case 5:
+  echo "It's 5";
+  break;
+  default:
+  echo "It's over 5";
+}
+
+?>
+</body>
+</html>

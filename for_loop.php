75c7596db1e5fc8048745d756ed79e301108c1c0
rayGreycloud <rayGreycloud@gmail.com>
2017-04-09 07:47:17 -0700

    lecture 18 for loops

---
+++ for_loop.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>For Loops</title>
+</head>
+<body>
+<?php
+
+for ($counter = 0; $counter < 10; $counter++) {
+  echo "Count: " . $counter . "<br>";
+
+}
+
+?>
+</body>
+</html>

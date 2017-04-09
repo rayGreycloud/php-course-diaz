607a8857faad915633667e3310848bf49beaec07
rayGreycloud <rayGreycloud@gmail.com>
2017-04-09 08:24:20 -0700

    practice section 3

--- _practical-app/3.php
+++ _practical-app/3.php
@@ -6,26 +6,57 @@
 	<aside class="col-xs-4">
 
 	<?php Navigation();?>
-			
+
 	</aside><!--SIDEBAR-->
 
 
 <article class="main-content col-xs-8">
 
-<?php  
+<?php
+$a = 5;
+$b = 3;
+$c = 1;
+if ($a < $b) {
+	echo "I hate PHP";
+} else if ($a < $c) {
+	echo "PHP...meh";
+} else {
+	echo "I love PHP kappa";
+};
+echo "<br>";
+
+for ($i = 0; $i <= 10; $i++) {
+	echo $i . "<br>";
+};
+
+$a = "apple";
+echo "It's a(n) ";
+switch($a) {
+	case "banana" :
+	echo "banana";
+	break;
+	case "orange" :
+	echo "orange";
+	break;
+	case "grape" :
+	echo "grape";
+	break;
+	case "apple" :
+	echo "apple";
+	break;
+	default :
+	echo "fruit";
+};
 
 /*  Step1: Make an if Statement with elseif and else to finally display string saying, I love PHP
 
+Step 2: Make a forloop  that displays 10 numbers
 
-
-	Step 2: Make a forloop  that displays 10 numbers
-
-
-	Step 3 : Make a switch Statement that test againts one condition with 5 cases
+Step 3 : Make a switch Statement that test againts one condition with 5 cases
 
  */
 
-	
+
 ?>
 
 
@@ -34,5 +65,5 @@
 
 
 </article><!--MAIN CONTENT-->
-	
+
 <?php include "includes/footer.php"; ?>

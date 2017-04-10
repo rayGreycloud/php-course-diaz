9fa74536224021f770d4df518cc17b9a1ae61bb7
rayGreycloud <rayGreycloud@gmail.com>
2017-04-10 09:01:33 -0700

    lecture 25 constants

---
+++ constants.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+$number = 10;
+echo $number . "<br>";
+$number = 1000;
+echo $number . "<br>";
+
+define("NAME", "Darth");
+
+echo NAME;
+?>
+
+</body>
+</html>

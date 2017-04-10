d6638ff2cc5692673076a1652203c13c3b772055
rayGreycloud <rayGreycloud@gmail.com>
2017-04-10 06:55:55 -0700

    lecture 23 returning values

---
+++ return_values.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+function addNumbers($number1, $number2) {
+
+  $sum = $number1 + $number2;
+
+  return $sum;
+}
+
+$result = addNumbers(12, 24);
+
+$result = addNumbers(100, $result);
+
+echo $result;
+
+?>
+
+</body>
+</html>

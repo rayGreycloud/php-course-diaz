6f4c70c7d096106e116dea6a9463a976a21f11e5
rayGreycloud <rayGreycloud@gmail.com>
2017-04-10 09:09:48 -0700

    Practice section 4

--- _practical-app/4.php
+++ _practical-app/4.php
@@ -6,15 +6,35 @@
 	<aside class="col-xs-4">
 
 		<?php Navigation();?>
-			
-		
+
+
 	</aside><!--SIDEBAR-->
 
 
 <article class="main-content col-xs-8">
 
-	
-	<?php  
+
+	<?php
+
+  function add() {
+
+    $sum = 1 + 2;
+
+    return $sum;
+  }
+
+  $result1 = add();
+  echo $result1 . "<br>";
+
+  function addNumbers($number1, $number2) {
+
+    $sum = $number1 + $number2;
+
+    return $sum;
+  }
+
+  $result2 = addNumbers(3, 4);
+  echo $result2
 
 /*  Step1: Define a function and make it return a calculation of 2 numbers
 
@@ -23,7 +43,7 @@
 
  */
 
-	
+
 ?>
 
 

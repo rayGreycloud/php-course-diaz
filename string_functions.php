54b9bf24cb2712d4653317943e6c8cc009331a1b
rayGreycloud <rayGreycloud@gmail.com>
2017-04-11 06:03:34 -0700

    lecture 28 string functions

---
+++ string_functions.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>String Functions</title>
+</head>
+<body>
+
+<?php
+
+$string = "Demoing string functions in PHP";
+// string length
+echo strlen($string);
+echo "<br>";
+// string to upper case
+echo strtoupper($string);
+echo "<br>";
+// string to lower case
+echo strtolower($string);
+echo "<br>";
+// print
+print $string;
+echo "<br>";
+
+
+?>
+</body>
+</html>

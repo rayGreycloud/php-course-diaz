adf53de3dd1d188f0ed1f3320281919726c28e0c
rayGreycloud <rayGreycloud@gmail.com>
2017-04-11 16:47:48 -0700

    lecture 32 extracting data from form

--- form.php
+++ form.php
@@ -7,15 +7,20 @@
 <body>
   <form action="form.php" method="post">
 
-    <input type="text" placeholder="Enter Username">
-    <input type="password" placeholder="Enter Password"><br>
+    <input type="text" name="username" placeholder="Enter Username">
+    <input type="password" name="password" placeholder="Enter Password"><br>
     <input type="submit" name="submit">
 
   </form>
 
 <?php
 if (isset($_POST['submit'])) {
-  echo "Working!";
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+echo "Hello " . $username . "<br>";
+echo "Your password is " . $password;
 }
 
  ?>

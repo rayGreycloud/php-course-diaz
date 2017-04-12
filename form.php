58a35676d976f81380a9abb0cbe87a69f6c9777f
rayGreycloud <rayGreycloud@gmail.com>
2017-04-11 17:06:44 -0700

    lecture 33 validating form values

--- form.php
+++ form.php
@@ -1,3 +1,32 @@
+<?php
+if (isset($_POST['submit'])) {
+
+  $names = array("Yoda", "Darth", "Obi-Wan", "Luke", "Leia", "Han", "Chewbacca");
+  $minimum = 5;
+  $maximum = 10;
+
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  if (strlen($username) < $minimum) {
+    echo "Username must be at least 5 characters" . "<br>";
+  }
+  if (strlen($username) > $maximum) {
+    echo "Username cannot exceed 10 characters" . "<br>";
+  }
+
+  if (!in_array($username, $names)) {
+    echo "Sorry, username not found";
+  } else {
+    echo "Welcome " . $username;
+  }
+
+// echo "Hello " . $username . "<br>";
+// echo "Your password is " . $password;
+}
+
+ ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,18 +41,6 @@
     <input type="submit" name="submit">
 
   </form>
-
-<?php
-if (isset($_POST['submit'])) {
-
-$username = $_POST['username'];
-$password = $_POST['password'];
-
-echo "Hello " . $username . "<br>";
-echo "Your password is " . $password;
-}
-
- ?>
 
 </body>
 </html>

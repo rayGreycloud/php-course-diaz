90e4a8adbf2538a7ae28834533060ffeff51a076
rayGreycloud <rayGreycloud@gmail.com>
2017-04-14 08:29:23 -0700

    Practice section 7

--- _practical-app/7.php
+++ _practical-app/7.php
@@ -1,22 +1,17 @@
 <?php include "functions.php"; ?>
 <?php include "includes/header.php";?>
-    
 
 	<section class="content">
 
 		<aside class="col-xs-4">
 
 		<?php Navigation();?>
-			
-			
+
 		</aside><!--SIDEBAR-->
 
-
 	<article class="main-content col-xs-8">
-	
-	
-	
-	<?php  
+
+	<?php
 
 	/*  Step 1 - Create a database in PHPmyadmin
 
@@ -27,13 +22,28 @@
 		Step 4 - Connect to Database and read data
 
 */
-	
-	?>
+$connection = mysqli_connect('localhost', 'root', '', 'practice7app');
 
+if (!$connection) {
+  die("Database connection failed");
+}
 
+$query = "SELECT * FROM users";
+$result = mysqli_query($connection, $query);
 
+if (!$result) {
+  die('Query FAILED: ' . mysqli_error());
+} else {
+  echo "Your Records:" . "<br>";
+}
 
+while($row = mysqli_fetch_assoc($result)) {
+?>
+  <pre>
+<?php print_r($row); ?>
+  </pre>
+<?php }	?>
 
-</article><!--MAIN CONTENT-->
+  </article><!--MAIN CONTENT-->
 
 <?php include "includes/footer.php"; ?>

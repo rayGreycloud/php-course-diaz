67a04ffdc5950217f19c694391ee99e75a383ba4
rayGreycloud <rayGreycloud@gmail.com>
2017-04-14 06:51:52 -0700

    Refactor and clean up login_create

--- mysql/login_create.php
+++ mysql/login_create.php
@@ -2,14 +2,8 @@
 <?php include "functions.php" ?>
 <?php createRows() ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Create</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-</head>
-<body>
+<?php include "./includes/header.php"; ?>
+
 <div class="container">
 
   <div class="col-sm-6">
@@ -31,5 +25,4 @@
 
 </div>
 
-</body>
-</html>
+<?php include "./includes/footer.php"; ?>

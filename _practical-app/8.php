78595174baff55481bb90fb003f2b78958e05da5
rayGreycloud <rayGreycloud@gmail.com>
2017-04-14 16:04:50 -0700

    Practice section 8

--- _practical-app/8.php
+++ _practical-app/8.php
@@ -6,16 +6,16 @@
 		<aside class="col-xs-4">
 
 		<?php Navigation();?>
-			
-			
+
+
 		</aside><!--SIDEBAR-->
 
 
-		
+
 	<article class="main-content col-xs-8">
-	
-	
-	<?php  
+
+
+	<?php
 
 	/*  Step 1 -Make a variable with some text as value
 
@@ -26,7 +26,12 @@
 		Step 4 - echo the variable
 
 	*/
-	
+	$foo = "someTextAsValue";
+  $hash = "$2y$10$";
+  $salt = "1234567890123456789012";
+  $hash_and_salt = $hash . $salt;
+  $bar = crypt($foo, $hash_and_salt);
+  echo $bar;
 	?>
 
 

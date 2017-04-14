ee885faa4d8db744c74c3ae42e299eb50c1061de
rayGreycloud <rayGreycloud@gmail.com>
2017-04-14 06:49:42 -0700

    Move readRows into functions

--- mysql/functions.php
+++ mysql/functions.php
@@ -42,6 +42,22 @@ function createRows() {
   }
 }
 
+function readRows() {
+  global $connection;
+  $query = "SELECT * FROM users";
+  $result = mysqli_query($connection, $query);
+
+  if (!$result) {
+    die('Query FAILED: ' . mysqli_error());
+  } else {
+    echo "Your Records:" . "<br>";
+  }
+
+  while($row = mysqli_fetch_assoc($result)) {
+    print_r($row);
+  }
+}
+
 function updateTable() {
   global $connection;
 

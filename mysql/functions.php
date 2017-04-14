983bfd5a1a2a5637e806f42cc6fcc242c139ed0d
rayGreycloud <rayGreycloud@gmail.com>
2017-04-14 14:30:28 -0700

    Add escape function to sanitize input

--- mysql/functions.php
+++ mysql/functions.php
@@ -28,6 +28,10 @@ function createRows() {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
+    // Sanitize to prevent SQL injection 
+    $username = mysqli_real_escape_string($connection, $username);
+    $password = mysqli_real_escape_string($connection, $password);
+
     $query = "INSERT INTO users(username, password) ";
 
     $query .= "VALUES ('$username', '$password')";

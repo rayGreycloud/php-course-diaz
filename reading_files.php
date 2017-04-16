e1b94a9b64be3de2d535b8dc856f2988003ef597
rayGreycloud <rayGreycloud@gmail.com>
2017-04-16 14:29:02 -0700

    lecture 77 reading files

---
+++ reading_files.php
@@ -0,0 +1,16 @@
+<?php
+
+$file = "example.txt";
+
+if ($handle = fopen($file, 'r')) {
+
+  echo $content = fread($handle, filesize($file));
+
+  fclose($handle);
+} else {
+
+  echo "Unable to read file";
+
+}
+
+?>
